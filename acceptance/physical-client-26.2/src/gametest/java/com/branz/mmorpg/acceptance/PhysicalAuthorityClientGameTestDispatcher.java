208ea3c1bc2ebeedaa1dadf863d9f580b3a04c3c
BranzX3 <bunzzbranzz@gmail.com>
2026-08-15 14:49:50 +0700

    Route physical consumable use acceptance mode

--- acceptance/physical-client-26.2/src/gametest/java/com/branz/mmorpg/acceptance/PhysicalAuthorityClientGameTestDispatcher.java
+++ acceptance/physical-client-26.2/src/gametest/java/com/branz/mmorpg/acceptance/PhysicalAuthorityClientGameTestDispatcher.java
@@ -7,6 +7,10 @@ import net.fabricmc.fabric.api.client.gametest.v1.context.ClientGameTestContext;
 public final class PhysicalAuthorityClientGameTestDispatcher implements FabricClientGameTest {
     @Override
     public void runTest(ClientGameTestContext context) {
+        if (Boolean.getBoolean("branz.acceptance.physicalConsumableUse")) {
+            new PhysicalConsumableUseClientGameTest().runTest(context);
+            return;
+        }
         if (Boolean.getBoolean("branz.acceptance.physicalConsumableLot")) {
             new PhysicalConsumableLotClientGameTest().runTest(context);
             return;

8bb770bc3d587438a9cf740c038fb77084983d31
BranzX3 <bunzzbranzz@gmail.com>
2026-08-19 03:26:51 +0700

    Move D13 unequip fixture clear after reconnect

--- acceptance/physical-client-26.2/src/gametest/java/com/branz/mmorpg/acceptance/PhysicalShieldD13ClientGameTest.java
+++ acceptance/physical-client-26.2/src/gametest/java/com/branz/mmorpg/acceptance/PhysicalShieldD13ClientGameTest.java
@@ -80,7 +80,6 @@ final class PhysicalShieldD13ClientGameTest {
         System.out.println("PHYSICAL_AUTHORITY_SHIELD_D13_SECOND_READY_CLIENT");
 
         freeHotbarSlot(context, SHIELD_HOTBAR_SLOT);
-        freeHotbarSlot(context, UNEQUIP_HOTBAR_SLOT);
 
         moveShield(context, FIRST_STORAGE_SLOT, SHIELD_HOTBAR_SLOT);
         Map<String, ItemAuthority> firstMoved =
@@ -201,6 +200,7 @@ final class PhysicalShieldD13ClientGameTest {
         }
         System.out.println("PHYSICAL_AUTHORITY_SHIELD_D13_RECONNECT_STABLE_CLIENT");
 
+        freeHotbarSlot(context, UNEQUIP_HOTBAR_SLOT);
         selectEmptyUnequipHotbar(context);
         context.getInput().pressKey(options -> options.keySwapOffhand);
         System.out.println("PHYSICAL_AUTHORITY_SHIELD_D13_UNEQUIP_F_SENT_CLIENT");
